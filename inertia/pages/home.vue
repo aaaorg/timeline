ca8cc80af208aafc2c15d3b2bf4c95e63177b577
Jakub Šindelář <megastary@proton.me>
2025-12-10 21:57:31 +0000

    init

---
+++ inertia/pages/home.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3'
+</script>
+
+<template>
+  <Head title="Homepage" />
+
+  <div
+    class="flex min-h-screen flex-col items-center justify-center bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 font-sans"
+  >
+    MTF TIMELINE
+  </div>
+</template>
