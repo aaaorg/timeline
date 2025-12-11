8e14fbbe83c4f9b0baa0a1b2a6af2b17498818d7
Jakub Šindelář <megastary@proton.me>
2025-12-11 16:21:02 +0000

    Add register page with auth layout

--- start/routes.ts
+++ start/routes.ts
@@ -10,3 +10,5 @@
 import router from '@adonisjs/core/services/router'
 router.on('/').renderInertia('home')
 router.on('/timeline').renderInertia('timeline')
+
+router.on('/register').renderInertia('auth/register')
